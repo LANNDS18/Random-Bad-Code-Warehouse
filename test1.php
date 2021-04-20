1e2c8b2d5c693ee7c0723f896c0784af4b746bd2
nicean <72734435+Niceanyh@users.noreply.github.com>
2021-04-20 11:54:00 +0100

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<title> my first page</title>
+</head>
+
+<body>
+<p>body hello,world.</p>
+<?php
+$test=[1,2,3];
+if ($test) {
+  echo "its just a git test//yes";
+  echo "night test successful";
+}
+
+ ?>
+</body>
+
+</html>
