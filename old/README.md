b52f07666406b3434b7cd0f07b34e81bf58a96b6
lannds18 <lannds18@icloud.com>
2021-04-26 11:41:41 +0100

    delete old version

--- old/README.md
+++
@@ -1,3 +0,0 @@
-# this is comp208 group project
-## pls clone the newest version before you develop our project
--its a pull request test; yeap!
--- old/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo '<html><head><Script Language="JavaScript">alert("Logout successfully!");</Script></head></html>'.
- "<meta http-equiv=\"refresh\" content=\"0;url=index.php\">";//redirect to register.html
- ?>
